f24dd9593f4a9cb8fe3ae3f7bfcb483bcf0954fe
Repos <347237845@qq.com>
2018-11-23 15:03:58 +0800

    更新注释
    更新注释

--- AttenData2Txt.py
+++ AttenData2Txt.py
@@ -6,37 +6,37 @@ from win32api import MessageBox
 from win32con import MB_OK
 from tkinter import filedialog
 
-filterwarnings("ignore")
 # 忽略警告
+filterwarnings("ignore")
 
+# 消除TK模块的小窗口
 root = Tk()
 root.withdraw()
-# 消除TK模块的小窗口
 
-file_path = filedialog.askopenfilename(filetypes=[('XLSX','xlsx')])
 # 文件路径提取（限制文件类型）
+file_path = filedialog.askopenfilename(filetypes=[('XLSX','xlsx')])
 
 if file_path.strip() == '':
-    MessageBox(0, '         未选择文件', '', MB_OK)
     # 第一个参数0，不知道啥意思，第二个参数是框体文本，第三个是标题文本，第四个是反馈键
+    MessageBox(0, '         未选择文件', '', MB_OK)
 
 else:
     try:
+        # 获取sheet页,此处是list对象
         wb1 = load_workbook(file_path, data_only=True)
         sheets1 = wb1.sheetnames
-        # 获取sheet页,此处是list对象
 
-        sheet1 = wb1[sheets1[0]]
         # 此处是sheet对象
+        sheet1 = wb1[sheets1[0]]
 
-        max_row = sheet1.max_row
         # 最大行数
+        max_row = sheet1.max_row
 
-        max_column = sheet1.max_column
         # 最大列数
+        max_column = sheet1.max_column
 
-        zlist = [2, 6, 10, 11, ]
         # 选取特定列（考勤号码、日期、签到时间、签退时间）
+        zlist = [2, 6, 10, 11, ]
 
         number_list = []
 
@@ -67,12 +67,12 @@ else:
                 f = 'P200001' + c + str(mat4.group(1)) + str(mat4.group(2)) + '00' + c + str(mat4.group(1)) + \
                     str(mat4.group(2)) + '00' + cell1
                 number_list.append(f)
-        wb1.close()
         # 关闭excel
+        wb1.close()
 
     except (TypeError,ValueError,AttributeError):
-        MessageBox(0, '         文档内容无效', '', MB_OK)
         # 异常处理
+        MessageBox(0, '         文档内容无效', '', MB_OK)
 
     else:
         filename = number_list[0][7:11] + '年' + number_list[0][12:13] + '月' + '考勤转换数据.txt'
